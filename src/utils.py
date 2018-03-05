19e8cb8ed5364e67957f6c1842bbd4396b919532
VictorSanh <victorsanh@gmail.com>
2018-03-05 17:39:15 +0100

    LSTM optimization not conclusive for the moment

--- src/utils.py
+++ src/utils.py
@@ -25,3 +25,8 @@ def preprocess(data_X_train, data_Y_train, portion_train=0.7, main_feature=False
 	Y_val = np.delete(Y, id_train, axis = 0).reshape(n_val,)
 
 	return X_train, Y_train, X_val, Y_val
+
+def extract_main_features(data_X_test):
+    filter_col = [col for col in data_X_test if col.startswith('miss') or col.startswith('offensive rebound') or col.startswith('score') or col.startswith('assist')]
+    data_X_test = data_X_test[filter_col]
+    return data_X_test.as_matrix()
